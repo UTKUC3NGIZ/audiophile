bf042868499157c88b1fa9f925a7fa37bf26ef0f
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-07-28 23:04:30 +0300

    basket modal

--- src/components/buttons/button1.js
+++ src/components/buttons/button1.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function button1() {
   return (
-    <button className="py-4 px-8 border bg-sunsetOrange border-transparent text-pureWhite font-bold text-sm uppercase font-sans hover:bg-peachy w-full">
-      See Product
+    <button className="py-3 px-8 border bg-sunsetOrange border-transparent text-pureWhite font-bold text-sm uppercase font-sans hover:bg-peachy w-full">
+      checkout
     </button>
   );
 }
