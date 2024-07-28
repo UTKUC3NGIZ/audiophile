05e362e6586253be4d55ce8d28b901ee5a82f072
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-07-28 23:33:35 +0300

    tablet hamburger

--- src/components/header.js
+++ src/components/header.js
@@ -49,58 +49,58 @@ function header() {
         className="relative z-10 focus:outline-none"
         onClose={() => hamburgerSetIsOpen(false)}
       >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto mt-28">
-          <div className="flex min-h-full md:min-h-max items-center justify-center p-4">
-            <DialogPanel
-              transition
-              className="w-full max-w-md md:max-w-max rounded-xl p-6  backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-            >
-              <div className="flex gap-12 md:gap-3 flex-col md:flex-row ">
-                <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
-                  <div className="absolute -top-10 ">
-                    <Image
-                      src="/assets/shared/desktop/image-category-thumbnail-headphones.png"
-                      alt=""
-                      width={130}
-                      height={160}
-                    />
-                  </div>
-                  <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
-                    <span className="text-deepBlack text-sm">Headphones</span>
-                    <Button3 />
-                  </div>
+        <DialogBackdrop className="fixed inset-0 md:bg-black/30" />
+
+        <div className="fixed flex w-screen items-center justify-center p-4 pt-28 bg-pureWhite md:rounded-b-lg">
+          <DialogPanel
+            transition
+            className="w-full max-w-md md:max-w-max rounded-xl  backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+          >
+            <div className="flex gap-12 md:gap-3 flex-col md:flex-row ">
+              <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
+                <div className="absolute -top-10 ">
+                  <Image
+                    src="/assets/shared/desktop/image-category-thumbnail-headphones.png"
+                    alt=""
+                    width={130}
+                    height={160}
+                  />
                 </div>
-                <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
-                  <div className="absolute -top-10 ">
-                    <Image
-                      src="/assets/shared/desktop/image-category-thumbnail-speakers.png"
-                      alt=""
-                      width={130}
-                      height={160}
-                    />
-                  </div>
-                  <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
-                    <span className="text-deepBlack text-sm">SPEAKERS</span>
-                    <Button3 />
-                  </div>
-                </div>
-                <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
-                  <div className="absolute -top-10 ">
-                    <Image
-                      src="/assets/shared/desktop/image-category-thumbnail-earphones.png"
-                      alt=""
-                      width={130}
-                      height={160}
-                    />
-                  </div>
-                  <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
-                    <span className="text-deepBlack text-sm">EARPHONES</span>
-                    <Button3 />
-                  </div>
+                <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
+                  <span className="text-deepBlack text-sm">Headphones</span>
+                  <Button3 />
                 </div>
               </div>
-            </DialogPanel>
-          </div>
+              <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
+                <div className="absolute -top-10 ">
+                  <Image
+                    src="/assets/shared/desktop/image-category-thumbnail-speakers.png"
+                    alt=""
+                    width={130}
+                    height={160}
+                  />
+                </div>
+                <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
+                  <span className="text-deepBlack text-sm">SPEAKERS</span>
+                  <Button3 />
+                </div>
+              </div>
+              <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
+                <div className="absolute -top-10 ">
+                  <Image
+                    src="/assets/shared/desktop/image-category-thumbnail-earphones.png"
+                    alt=""
+                    width={130}
+                    height={160}
+                  />
+                </div>
+                <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
+                  <span className="text-deepBlack text-sm">EARPHONES</span>
+                  <Button3 />
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
         </div>
       </Dialog>
 
