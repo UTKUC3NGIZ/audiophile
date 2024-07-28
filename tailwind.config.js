d00cdbe5fc7079908396fc6c948669c7f8d4139f
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-07-28 16:08:10 +0300

    default settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        sunsetOrange: "#D87D4A",
+        midnightBlack: "#101010",
+        cloudGray: "#F1F1F1",
+        snowWhite: "#FAFAFA",
+        peachy: "#FBAF85",
+        pureWhite: "#FFFFFF",
+        deepBlack: "#000000",
       },
     },
   },
