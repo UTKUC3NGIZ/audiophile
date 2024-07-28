63e16d1edc577cadbe7ddddfe3f3bc9fffc88a75
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-07-28 18:18:06 +0300

    hamburger menu

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Button1 from "@/components/buttons/button1";
 import Button2 from "@/components/buttons/button2";
 import Button3 from "@/components/buttons/button3";
 export default function Home() {
-  return <main></main>;
+  return <main className=""></main>;
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import Button3 from "@/components/buttons/button3";
+import { useState } from "react";
 
 function header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-between py-8 px-6 ">
-      <button>
+    <div className="flex justify-between py-8 px-6 bg-deepBlack">
+      <button onClick={() => setIsOpen(true)}>
         <Image
           src="/assets/shared/tablet/icon-hamburger.svg"
           alt="Arrow Right Icon"
@@ -28,6 +34,67 @@ function header() {
           height={20}
         />
       </button>
+      {/* Modal Dialog */}
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={() => setIsOpen(false)}
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto mt-28">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl p-6  backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
+              <div className="flex gap-12 flex-col ">
+                <div className="bg-cloudGray flex items-center flex-col rounded-lg relative">
+                  <div className="absolute -top-10 ">
+                    <Image
+                      src="/assets/shared/desktop/image-category-thumbnail-headphones.png"
+                      alt=""
+                      width={130}
+                      height={160}
+                    />
+                  </div>
+                  <div className="pt-12 text-center font-bold uppercase mt-9">
+                    <span className="text-deepBlack text-sm">Headphones</span>
+                    <Button3 />
+                  </div>
+                </div>
+                <div className="bg-cloudGray flex items-center flex-col rounded-lg relative">
+                  <div className="absolute -top-10 ">
+                    <Image
+                      src="/assets/shared/desktop/image-category-thumbnail-speakers.png"
+                      alt=""
+                      width={130}
+                      height={160}
+                    />
+                  </div>
+                  <div className="pt-12 text-center font-bold uppercase mt-9">
+                    <span className="text-deepBlack text-sm">SPEAKERS</span>
+                    <Button3 />
+                  </div>
+                </div>
+                <div className="bg-cloudGray flex items-center flex-col rounded-lg relative">
+                  <div className="absolute -top-10 ">
+                    <Image
+                      src="/assets/shared/desktop/image-category-thumbnail-earphones.png"
+                      alt=""
+                      width={130}
+                      height={160}
+                    />
+                  </div>
+                  <div className="pt-12 text-center font-bold uppercase mt-9">
+                    <span className="text-deepBlack text-sm">EARPHONES</span>
+                    <Button3 />
+                  </div>
+                </div>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 }
