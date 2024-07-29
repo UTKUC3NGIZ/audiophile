02a289b17923ed1c03a477130a73627e154c0bf2
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-07-30 00:10:12 +0300

    homepage category

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 "use client";
 import Button1 from "@/components/buttons/button1";
+import Button3 from "@/components/buttons/button3";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main class="relative w-full h-screen overflow-hidden">
+    <main class="relative w-full h-screen px-6">
       <header>
         {/* background */}
         <img
@@ -22,7 +24,7 @@ export default function Home() {
           class="absolute inset-0 w-full h-auto object-cover hidden md:block -z-10"
         />
         {/* Main Content */}
-        <div className="text-center flex justify-center items-center flex-col pt-28 px-6">
+        <div className="text-center flex justify-center items-center flex-col pt-28">
           <span className="text-pureWhite text-opacity-50 text-base text-center pb-4 tracking-[.5em] font-light uppercase">
             NEW PRODUCT
           </span>
@@ -38,6 +40,50 @@ export default function Home() {
           </div>
         </div>
       </header>
+      <div className="flex gap-12 md:gap-3 flex-col md:flex-row pt-64">
+        <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
+          <div className="absolute -top-10 ">
+            <Image
+              src="/assets/shared/desktop/image-category-thumbnail-headphones.png"
+              alt=""
+              width={130}
+              height={160}
+            />
+          </div>
+          <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
+            <span className="text-deepBlack text-sm">Headphones</span>
+            <Button3 />
+          </div>
+        </div>
+        <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
+          <div className="absolute -top-10 ">
+            <Image
+              src="/assets/shared/desktop/image-category-thumbnail-speakers.png"
+              alt=""
+              width={130}
+              height={160}
+            />
+          </div>
+          <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
+            <span className="text-deepBlack text-sm">SPEAKERS</span>
+            <Button3 />
+          </div>
+        </div>
+        <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
+          <div className="absolute -top-10 ">
+            <Image
+              src="/assets/shared/desktop/image-category-thumbnail-earphones.png"
+              alt=""
+              width={130}
+              height={160}
+            />
+          </div>
+          <div className="pt-12 text-center font-bold uppercase mt-9 flex flex-col">
+            <span className="text-deepBlack text-sm">EARPHONES</span>
+            <Button3 />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
