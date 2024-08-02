fb62875ba33dcb4dde00bdb299dd18dbe1b7bea9
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-02 17:11:14 +0300

    main page desktop

--- src/app/page.js
+++ src/app/page.js
@@ -25,24 +25,29 @@ export default function Home() {
           class="absolute inset-0 w-full h-auto object-cover hidden lg:block -z-10"
         />
         {/* Main Content */}
-        <div className="text-center flex justify-center items-center flex-col pt-28 md:pt-60">
-          <span className="text-pureWhite text-opacity-50 text-base text-center pb-4 tracking-[.5em] font-light uppercase">
+        <div className="text-center flex justify-center items-center flex-col pt-28 md:pt-60 lg:pt-32">
+          <span className="text-pureWhite text-opacity-50 text-base lg:text-start w-full text-center pb-4 tracking-[.5em] font-light uppercase">
             NEW PRODUCT
           </span>
-          <h1 className="text-pureWhite text-4xl md:text-6xl text-center pb-6 font-bold uppercase">
-            XX99 Mark II HeadphoneS
+          <h1 className="text-pureWhite text-4xl md:text-6xl text-center lg:text-start pb-6 font-bold uppercase lg:w-full">
+            XX99 Mark II <br className="hidden lg:block" /> HeadphoneS
           </h1>
-          <h3 className="text-pureWhite text-opacity-75 text-base text-center pb-7 font-medium md:w-3/5">
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
-          </h3>
-          <div>
-            <Button1 content={"See Product"} />
+          <div className="flex justify-center lg:justify-start lg:w-full">
+            <h3 className="text-pureWhite text-opacity-75 text-base text-center lg:text-start pb-7 font-medium md:w-3/5">
+              Experience natural, lifelike audio and exceptional{" "}
+              <br className="hidden lg:block" /> build quality made for the
+              passionate music enthusiast.
+            </h3>
+          </div>
+          <div className="lg:w-full">
+            <div className="flex w-fit">
+              <Button1 content={"See Product"} />
+            </div>
           </div>
         </div>
       </header>
       {/* Menu */}
-      <div className="flex gap-12 md:gap-3 flex-col md:flex-row pt-64 md:pt-72">
+      <div className="flex gap-12 md:gap-3 flex-col md:flex-row pt-64 md:pt-72 lg:pt-96">
         <div className="bg-cloudGray flex items-center flex-col rounded-lg relative md:w-screen">
           <div className="absolute -top-10 ">
             <Image
@@ -87,35 +92,46 @@ export default function Home() {
         </div>
       </div>
       {/* See Product */}
-      <div className="bg-sunsetOrange rounded-lg mt-[120px] md:mt-24 ">
-        <div className="relative flex justify-center ">
+      <div className="bg-sunsetOrange rounded-lg mt-[120px] md:mt-24 lg:flex lg:flex-row relative overflow-hidden">
+        <div className=" flex justify-center ">
           <img
             src="/assets/home/desktop/pattern-circles.svg"
             alt=""
-            className="absolute w-[558px] h-[558px] md:w-[944px] md:h-[944px] object-cover -top-[120px] md:-top-[280px]"
+            className="absolute w-[558px] h-[558px] md:w-[944px] md:h-[944px] object-cover -top-[120px] md:-top-[280px] lg:-top-[120px] lg:-left-36"
           />
           <img
             src="/assets/home/mobile/image-speaker-zx9.png"
             alt=""
-            className=" w-[170px] md:w-[197px] h-auto mt-12"
+            className=" w-[170px] md:w-[197px] h-auto mt-12 lg:hidden"
+          />
+          <img
+            src="/assets/home/desktop/image-speaker-zx9.png"
+            alt=""
+            className="w-[410px] h-auto mt-12 hidden lg:block absolute top-4 z-10 left-0 ml-32"
           />
         </div>
-        <div className="flex flex-col items-center text-center pt-8 md:pt-16 pb-14 gap-6">
-          <h2 className="text-4xl md:text-6xl font-bold uppercase text-pureWhite">
-            ZX9 <br />
-            SPEAKER
-          </h2>
-          <h3 className="text-base text-pureWhite md:w-2/5">
-            Upgrade to premium speakers that are phenomenally built to deliver
-            truly remarkable sound.
-          </h3>
-          <div>
-            <Button1
-              content={"See Product"}
-              bgColor={"midnightBlack"}
-              textColor={"pureWhite"}
-              borderColor={"transparent"}
-            />
+        <div className="lg:flex lg:justify-end lg:w-full">
+          <div className="lg:w-1/2">
+            <div className="flex flex-col items-center text-center pt-8 md:pt-16 pb-14 gap-6 lg:justify-center lg:w-full lg:pt-[133px] lg:pb-32 lg:pl-32 lg:pr-24 ">
+              <h2 className="text-4xl md:text-6xl font-bold uppercase text-pureWhite lg:text-start lg:w-full">
+                ZX9 <br />
+                SPEAKER
+              </h2>
+              <h3 className="text-base text-pureWhite md:w-2/5 lg:text-start lg:w-full">
+                Upgrade to premium speakers that are phenomenally built to
+                deliver truly remarkable sound.
+              </h3>
+              <div className=" lg:mx-auto lg:text-start lg:w-full">
+                <div className="w-fit">
+                  <Button1
+                    content={"See Product"}
+                    bgColor={"midnightBlack"}
+                    textColor={"pureWhite"}
+                    borderColor={"transparent"}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -129,7 +145,12 @@ export default function Home() {
         <img
           src="/assets/home/tablet/image-speaker-zx7.jpg"
           alt=""
-          className="w-full h-auto hidden md:block"
+          className="w-full h-auto hidden md:block lg:hidden"
+        />
+        <img
+          src="/assets/home/desktop/image-speaker-zx7.jpg"
+          alt=""
+          className="w-full h-auto hidden lg:block"
         />
         <div
           className="flex flex-col absolute top-1/2 ml-6 md:ml-16 transform  -translate-y-1/2
@@ -158,7 +179,12 @@ export default function Home() {
         <img
           src="/assets/home/tablet/image-earphones-yx1.jpg"
           alt=""
-          className="w-full h-auto rounded-lg hidden md:block"
+          className="w-full h-auto rounded-lg hidden md:block lg:hidden"
+        />
+        <img
+          src="/assets/home/desktop/image-earphones-yx1.jpg"
+          alt=""
+          className="w-full h-auto rounded-lg hidden lg:block"
         />
         <div className="flex flex-col  border rounded-lg w-full bg-cloudGray md:justify-center">
           <div className="my-10 ml-6 md:ml-10">
@@ -178,7 +204,7 @@ export default function Home() {
       </div>
       {/* Content */}
 
-      <div className="my-[120px] md:my-24">
+      <div className="my-[120px] md:my-24 lg:flex lg:flex-row">
         <img
           src="/assets/shared/mobile/image-best-gear.jpg"
           alt=""
@@ -187,20 +213,30 @@ export default function Home() {
         <img
           src="/assets/shared/tablet/image-best-gear.jpg"
           alt=""
-          className="w-full h-auto rounded-lg hidden md:block"
+          className="w-full h-auto rounded-lg hidden md:block lg:hidden "
         />
-        <h2 className="text-deepBlack text-3xl md:text-[40px] md:w-4/5 md:mx-auto font-bold text-center mt-10 mb-8 uppercase">
-          Bringing you the <span className="text-sunsetOrange">best</span> audio
-          gear
-        </h2>
-        <p className="text-deepBlack text-opacity-50 text-base text-center font-normal">
-          Located at the heart of New York City, Audiophile is the premier store
-          for high end headphones, earphones, speakers, and audio accessories.
-          We have a large showroom and luxury demonstration rooms available for
-          you to browse and experience a wide range of our products. Stop by our
-          store to meet some of the fantastic people who make Audiophile the
-          best place to buy your portable audio equipment.
-        </p>
+        <div className="flex flex-col lg:flex-row lg:grid lg:grid-cols-2 lg:gap-32">
+          <img
+            src="/assets/shared/desktop/image-best-gear.jpg"
+            alt=""
+            className="w-full h-auto rounded-lg hidden lg:block lg:order-2 "
+          />
+          <div className="flex flex-col lg:order-1 lg:justify-center">
+            <h2 className="text-deepBlack text-3xl md:text-[40px] md:w-4/5 md:mx-auto lg:mx-0 font-bold text-center mt-10 mb-8 uppercase lg:text-start">
+              Bringing you the <span className="text-sunsetOrange">best</span>{" "}
+              audio gear
+            </h2>
+            <p className="text-deepBlack text-opacity-50 text-base text-center font-normal lg:text-start">
+              Located at the heart of New York City, Audiophile is the premier
+              store for high end headphones, earphones, speakers, and audio
+              accessories. We have a large showroom and luxury demonstration
+              rooms available for you to browse and experience a wide range of
+              our products. Stop by our store to meet some of the fantastic
+              people who make Audiophile the best place to buy your portable
+              audio equipment.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
