90b0017a1972757db74bc8fa2def856f47d29404
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-18 03:30:57 +0300

    bug fix

--- src/app/category/client.js
+++ src/app/category/client.js
@@ -1,9 +1,29 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Button1 from "@/components/buttons/button1";
+import Link from "next/link";
 function page({ product }) {
+  const [deviceType, setDeviceType] = useState("mobile");
+
   useEffect(() => {
-    console.log(product);
+    const updateDeviceType = () => {
+      const screen = window.matchMedia("(min-width: 768px)");
+      if (screen.matches) {
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+          setDeviceType("desktop");
+        } else {
+          setDeviceType("tablet");
+        }
+      } else {
+        setDeviceType("mobile");
+      }
+    };
+
+    updateDeviceType();
+
+    window.addEventListener("resize", updateDeviceType);
+
+    return () => window.removeEventListener("resize", updateDeviceType);
   }, [product]);
   return (
     <div>
@@ -13,108 +33,35 @@ function page({ product }) {
         </h1>
       </div>
       <div className="flex flex-col gap-14 pb-20">
-        <div className="px-6 md:px-10 lg:px-40 flex lg:grid lg:grid-cols-2 flex-col gap-8 lg:gap-36 text-center pt-16">
-          <img
-            src="/assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg md:hidden"
-          />
-          <img
-            src="/assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg hidden md:block lg:hidden"
-          />
-          <img
-            src="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg hidden lg:block hidden"
-          />
-          <div className="flex flex-col gap-6 items-center lg:justify-center lg:items-start">
-            <span className="text-sunsetOrange text-sm font-light tracking-[.6em]">
-              NEW PRODUCT
-            </span>
-            <h1 className="md:text-4xl text-3xl text-deepBlack font-bold lg:text-left">
-              XX99 Mark II <br /> Headphones
-            </h1>
-            <p className="text-deepBlack text-opacity-50 text-base text-center font-normal lg:text-start">
-              As the gold standard for headphones, the classic XX99 Mark I
-              offers detailed and accurate audio reproduction for audiophiles,
-              mixing engineers, and music aficionados alike in studios and on
-              the go.
-            </p>
-            <div className="w-fit">
-              <Button1 color={"orange"} content={"See Product"} />
+        {product.map((item, index) => (
+          <div
+            className="px-6 md:px-10 lg:px-40 flex lg:grid lg:grid-cols-2 flex-col gap-8 lg:gap-36 text-center pt-16"
+            key={index}
+          >
+            <img
+              src={item.categoryImage[deviceType]}
+              alt="Mobile Header"
+              className=" w-auto h-auto object-cover rounded-lg "
+            />
+            <div className="flex flex-col gap-6 items-center lg:justify-center lg:items-start">
+              <span className="text-sunsetOrange text-sm font-light tracking-[.6em] uppercase">
+                {item.new ? "New Product" : ""}
+              </span>
+              <h1 className="md:text-4xl text-3xl text-deepBlack font-bold lg:text-left">
+                {item.name}
+              </h1>
+              <p className="text-deepBlack text-opacity-50 text-base text-center font-normal lg:text-start">
+                {item.description}
+              </p>
+              <div className="w-fit">
+                <Link href={`/product-detail/${item.id}`}>
+                  {" "}
+                  <Button1 color={"orange"} content={"See Product"} />{" "}
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="px-6 md:px-10 lg:px-40 flex lg:grid lg:grid-cols-2 flex-col gap-8 lg:gap-36 text-center pt-16">
-          <img
-            src="/assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg md:hidden"
-          />
-          <img
-            src="/assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg hidden md:block lg:hidden"
-          />
-          <img
-            src="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg hidden lg:block hidden order-2"
-          />
-          <div className="flex flex-col gap-6 items-center lg:justify-center lg:items-start">
-            <span className="text-sunsetOrange text-sm font-light tracking-[.6em]">
-              NEW PRODUCT
-            </span>
-            <h1 className="md:text-4xl text-3xl text-deepBlack font-bold lg:text-left">
-              XX99 Mark II <br /> Headphones
-            </h1>
-            <p className="text-deepBlack text-opacity-50 text-base text-center font-normal lg:text-start">
-              As the gold standard for headphones, the classic XX99 Mark I
-              offers detailed and accurate audio reproduction for audiophiles,
-              mixing engineers, and music aficionados alike in studios and on
-              the go.
-            </p>
-            <div className="w-fit">
-              <Button1 color={"orange"} content={"See Product"} />
-            </div>
-          </div>
-        </div>
-        <div className="px-6 md:px-10 lg:px-40 flex lg:grid lg:grid-cols-2 flex-col gap-8 lg:gap-36 text-center pt-16">
-          <img
-            src="/assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg md:hidden"
-          />
-          <img
-            src="/assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg hidden md:block lg:hidden"
-          />
-          <img
-            src="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
-            alt="Mobile Header"
-            className=" w-auto h-auto object-cover rounded-lg hidden lg:block hidden"
-          />
-          <div className="flex flex-col gap-6 items-center lg:justify-center lg:items-start">
-            <span className="text-sunsetOrange text-sm font-light tracking-[.6em]">
-              NEW PRODUCT
-            </span>
-            <h1 className="md:text-4xl text-3xl text-deepBlack font-bold lg:text-left">
-              XX99 Mark II <br /> Headphones
-            </h1>
-            <p className="text-deepBlack text-opacity-50 text-base text-center font-normal lg:text-start">
-              As the gold standard for headphones, the classic XX99 Mark I
-              offers detailed and accurate audio reproduction for audiophiles,
-              mixing engineers, and music aficionados alike in studios and on
-              the go.
-            </p>
-            <div className="w-fit">
-              <Button1 color={"orange"} content={"See Product"} />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
