dfb418f6fd73ea891b068aebdc8fd7fe1c94bf11
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-18 07:04:55 +0300

    bgfx

--- src/app/product-detail/[product]/page.js
+++ src/app/product-detail/[product]/page.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ClientComponent from "@/app/product-detail/client";
 import { promises as fs } from "fs";
+import path from "path";
 import Content from "@/components/content";
 import Menu from "@/components/menu";
+
 export default async function Page({ params }) {
-  const file = await fs.readFile(process.cwd() + "/src/data.json", "utf8");
+  const file = await fs.readFile(
+    path.join(process.cwd(), "src/data.json"),
+    "utf8"
+  );
   const data = JSON.parse(file);
   const product = data.find((item) => item.slug === params.product);
 
