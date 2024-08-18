637b1c44ca8c2240791a59ba307fa30442cf766d
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-18 06:26:29 +0300

    bg

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Toaster />
         <Header />
-        {children}
-        <Footer />
+        {/* {children}
+        <Footer /> */}
       </body>
     </html>
   );
