9991e7e96db3745a8da99f069499a0d5e055a610
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-18 06:27:33 +0300

    bg fx

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {/* {children}
-        <Footer /> */}
+        {/* <Header /> */}
+        {children}
+        {/* <Footer /> */}
       </body>
     </html>
   );
